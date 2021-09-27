96fad29896e2a6b1d8034e180f88fbd41efd9515
Ed Catmur <ed@catmur.uk>
2021-09-27 09:55:14 +0100

    simplify

--- gamedev-sample.cpp
+++ gamedev-sample.cpp
@@ -25,16 +25,15 @@
 #include <stdexcept>
 #include <string>
 #include <typeinfo>
+#include <vector>
 
 //------------------------------------------------------------------------------------------------------------------------------
 //! These definitions are based on assembly listings produded by the compiler (/FAs) rather than built-in ones
 //! @{
 
 #pragma pack (push, 4)
-
 namespace CORRECT
 {
-
     struct CatchableType
     {
         __int32 properties;
@@ -51,7 +50,6 @@ namespace CORRECT
         __int32 pForwardCompat;
         __int32 pCatchableTypeArray;
     };
-
 }
 #pragma pack (pop)
 
@@ -78,67 +76,74 @@ private:
     SymInit(const SymInit &) = delete;
     SymInit & operator=(const SymInit &) = delete;
 };
-std::string get_module_path(HMODULE module = 0) {
-    char path_name[MAX_PATH] = {};
-    DWORD size = GetModuleFileNameA(module, path_name, MAX_PATH);
-    return std::string(path_name, size);
-}
-void write_module_name(std::ostream & os, HANDLE process, DWORD64 program_counter) {
-    DWORD64 module_base = SymGetModuleBase64(process, program_counter);
-    if (module_base) {
-        std::string module_name = get_module_path(reinterpret_cast<HMODULE>(module_base));
-        if (!module_name.empty())
-            os << module_name << "|";
-        else
-            os << "Unknown module|";
-    } else {
-        os << "Unknown module|";
-    }
-}
-void write_function_name(std::ostream & os, HANDLE process, DWORD64 program_counter) {
-    SYMBOL_INFO_PACKAGE sym = { sizeof(sym.si) };
-    sym.si.MaxNameLen = MAX_SYM_NAME;
-    if (SymFromAddr(process, program_counter, 0, &sym.si)) {
-        os << sym.si.Name << "()";
-    } else {
-        os << "???";
-    }
-}
-void write_file_and_line(std::ostream & os, HANDLE process, DWORD64 program_counter) {
-    IMAGEHLP_LINE64 ih_line = { sizeof(IMAGEHLP_LINE64) };
-    DWORD dummy = 0;
-    if (SymGetLineFromAddr64(process, program_counter, &dummy, &ih_line)) {
-        os << "|" << ih_line.FileName << ":" << ih_line.LineNumber;
-    }
-}
-void generate_stack_trace(std::ostream & os, CONTEXT ctx, int skip) {
-    STACKFRAME64 sf = {};
-    sf.AddrPC.Offset = ctx.Rip;
-    sf.AddrPC.Mode = AddrModeFlat;
-    sf.AddrStack.Offset = ctx.Rsp;
-    sf.AddrStack.Mode = AddrModeFlat;
-    sf.AddrFrame.Offset = ctx.Rbp;
-    sf.AddrFrame.Mode = AddrModeFlat;
-    HANDLE process = GetCurrentProcess();
-    HANDLE thread = GetCurrentThread();
-    os << std::uppercase;
-    for (;;) {
-        SetLastError(0);
-        BOOL stack_walk_ok = StackWalk64(IMAGE_FILE_MACHINE_AMD64, process, thread, &sf, &ctx, 0, &SymFunctionTableAccess64, &SymGetModuleBase64, 0);
-        if (!stack_walk_ok || !sf.AddrFrame.Offset)
-            return;
-        if (skip) {
-            --skip;
-        } else {
-            // write the address
-            os << std::hex << static_cast<std::uintptr_t>(sf.AddrPC.Offset) << "|" << std::dec;
-            write_module_name(os, process, sf.AddrPC.Offset);
-            write_function_name(os, process, sf.AddrPC.Offset);
-            write_file_and_line(os, process, sf.AddrPC.Offset);
-            os << "\n";
+
+struct StackTrace {
+    StackTrace() = default;
+
+    HANDLE process = nullptr;
+    std::vector<DWORD64> pc;
+
+    void generate(CONTEXT ctx, int skip) {
+        STACKFRAME64 sf = {};
+        sf.AddrPC.Offset = ctx.Rip;
+        sf.AddrPC.Mode = AddrModeFlat;
+        sf.AddrStack.Offset = ctx.Rsp;
+        sf.AddrStack.Mode = AddrModeFlat;
+        sf.AddrFrame.Offset = ctx.Rbp;
+        sf.AddrFrame.Mode = AddrModeFlat;
+        process = GetCurrentProcess();
+        HANDLE thread = GetCurrentThread();
+        for (;;) {
+            SetLastError(0);
+            BOOL stack_walk_ok = StackWalk64(IMAGE_FILE_MACHINE_AMD64, process, thread, &sf, &ctx, 0, &SymFunctionTableAccess64, &SymGetModuleBase64, 0);
+            if (!stack_walk_ok || !sf.AddrFrame.Offset)
+                return;
+            if (skip)
+                --skip;
+            else
+                pc.push_back(sf.AddrPC.Offset);
         }
     }
-}
+
+    friend std::ostream& operator<<(std::ostream& os, StackTrace const& st) {
+        os << std::uppercase;
+        for (auto const pc : st.pc) {
+            // write the address
+            os << std::hex << static_cast<std::uintptr_t>(pc) << "|" << std::dec;
+            // write module name
+            DWORD64 module_base = SymGetModuleBase64(st.process, pc);
+            if (module_base) {
+                char path_name[MAX_PATH] = {};
+                DWORD size = GetModuleFileNameA(reinterpret_cast<HMODULE>(module_base), path_name, MAX_PATH);
+                if (size)
+                    os << path_name << "|";
+                else
+                    os << "???|";
+            }
+            else {
+                os << "???|";
+            }
+            // write function name
+            SYMBOL_INFO_PACKAGE sym = { sizeof(sym.si) };
+            sym.si.MaxNameLen = MAX_SYM_NAME;
+            if (SymFromAddr(st.process, pc, 0, &sym.si)) {
+                os << sym.si.Name << "()";
+            }
+            else {
+                os << "???";
+            }
+            // write source location
+            IMAGEHLP_LINE64 ih_line = { sizeof(IMAGEHLP_LINE64) };
+            DWORD dummy = 0;
+            if (SymGetLineFromAddr64(st.process, pc, &dummy, &ih_line)) {
+                os << "|" << ih_line.FileName << ":" << ih_line.LineNumber;
+            }
+            os << "\n";
+        }
+        return os;
+    }
+};
+
 struct UntypedException {
 #define RVA_TO_VA_(type, addr)  ( (type) ((uintptr_t) module + (uintptr_t) (addr)) )
     UntypedException(const EXCEPTION_RECORD& er) :
@@ -157,16 +162,23 @@ struct UntypedException {
 
     unsigned getNumCatchableTypes() const { return cArray ? cArray->nCatchableTypes : 0u; }
 
-    std::type_info const* getTypeInfo(unsigned i) const
-    {
-        const CORRECT::CatchableType* cType = RVA_TO_VA_(const CORRECT::CatchableType*, ((__int32*)(cArray->arrayOfCatchableTypes))[i]);
-        return RVA_TO_VA_(const std::type_info*, cType->pType);
+    CORRECT::CatchableType const* cType(unsigned i) const {
+        return RVA_TO_VA_(const CORRECT::CatchableType*, ((__int32*)(cArray->arrayOfCatchableTypes))[i]);
     }
 
-    unsigned getThisDisplacement(unsigned i) const
+    std::type_info const* getTypeInfo(unsigned i) const
     {
-        const CORRECT::CatchableType* cType = RVA_TO_VA_(const CORRECT::CatchableType*, ((__int32*)(cArray->arrayOfCatchableTypes))[i]);
-        return cType->thisDisplacement.mdisp;
+        return RVA_TO_VA_(const std::type_info*, cType(i)->pType);
+    }
+    unsigned getThisDisplacement(unsigned i) const { return cType(i)->thisDisplacement.mdisp; }
+
+    char* copy(unsigned i) const {
+        auto ct = cType(i);
+        // FIXME leak, hack
+        auto data = new char[ct->sizeOrOffset];
+        auto copy = RVA_TO_VA_(void (*)(void*, void*), cType(i)->copyFunction);
+        copy(data, exception_object);
+        return data;
     }
 
     void * exception_object;
@@ -176,94 +188,58 @@ struct UntypedException {
     const _CatchableTypeArray* cArray = nullptr;
 #undef RVA_TO_VA_
 };
-void * exception_cast_worker(const UntypedException & e, const type_info & ti) {
-    for (int i = 0; i < e.getNumCatchableTypes(); ++i) {
-        const std::type_info & ti_i = *e.getTypeInfo(i);
-        if (ti_i == ti)
-            return reinterpret_cast<char*>(e.exception_object) + e.getThisDisplacement(i);
-    }
-    return nullptr;
-}
-void get_exception_types(std::ostream & os, const UntypedException & e) {
+template<class T> T * exception_cast(const UntypedException & e, bool copy) {
+    const std::type_info & ti = typeid(T);
     for (int i = 0; i < e.getNumCatchableTypes(); ++i) {
         const std::type_info& ti_i = *e.getTypeInfo(i);
-        os << ti_i.name() << "\n";
-    }
-}
-template<class T> T * exception_cast(const UntypedException & e) {
-    const std::type_info & ti = typeid(T);
-    return reinterpret_cast<T*>(exception_cast_worker(e, ti));
-}
-DWORD do_filter(EXCEPTION_POINTERS * eps, std::string & buffer) {
-    std::ostringstream sstr;
-    const EXCEPTION_RECORD & er = *eps->ExceptionRecord;
-    int skip = 0;
-    switch (er.ExceptionCode) {
-    case EXCEPTION_CPP_MICROSOFT: { // C++ exception
-        UntypedException ue(er);
-        if (std::exception* e = exception_cast<std::exception>(ue)) {
-            const std::type_info & ti = typeid(*e);
-            sstr << ti.name() << ":" << e->what();
-        } else {
-            sstr << "Unknown C++ exception thrown.\n";
-            get_exception_types(sstr, ue);
+        if (ti_i == ti) {
+            if (copy)
+                return reinterpret_cast<T*>(e.copy(i));
+            else
+                return reinterpret_cast<T*>(e.exception_object) + e.getThisDisplacement(i);
         }
-        skip = 2; // skip RaiseException and _CxxThrowException
     }
-    break;
-    case EXCEPTION_ACCESS_VIOLATION: {
-        sstr << "Access violation. Illegal " << (er.ExceptionInformation[0] ? "write" : "read") <<
-            " by " << er.ExceptionAddress <<
-            " at " << static_cast<std::uintptr_t>(er.ExceptionInformation[1]);
-    }
-    break;
-    default: {
-        sstr << "SEH exception thrown. Exception code: " << std::hex << std::uppercase << er.ExceptionCode <<
-            " at " << er.ExceptionAddress;
-    }
-    }
-    sstr << "\n\nStack Trace:\n";
-    generate_stack_trace(sstr, *eps->ContextRecord, skip);
-    buffer = sstr.str();
-    return EXCEPTION_EXECUTE_HANDLER;
+    return nullptr;    
 }
-DWORD filter(EXCEPTION_POINTERS * eps, std::string & buffer) {
-    __try {
-        return do_filter(eps, buffer);
-    } __except (EXCEPTION_EXECUTE_HANDLER) {
-        return EXCEPTION_CONTINUE_SEARCH;
-    }
-}
-int actual_main(int, char **) {
-    // do stuff
-    // cause an access violation
-    //char * ptr = 0; *ptr = 0;
-    // divide by zero
-    //int x = 5; x = x / (x - x);
-    // C++ exception
-    throw std::runtime_error("I'm an exception!");
-    //throw 5;
-    return 0;
-}
-void save_buffer(const std::string & buffer) {
-    std::ofstream ofs("err_log.txt");
-    if (ofs)
-        ofs << buffer;
-}
-int seh_helper(int argc, char ** argv, std::string & buffer) {
-    __try {
-        return actual_main(argc, argv);
-    }
-    __except (filter(GetExceptionInformation(), buffer)) {
-        if (!buffer.empty()) {
-            save_buffer(buffer);
-            MessageBoxA(0, buffer.c_str(), "Abnormal Termination", MB_OK);
+
+template<class Ex>
+auto tryCatch(auto f, auto e) {
+    StackTrace st;
+    Ex* ex = nullptr;
+    return [&] {
+        __try {
+            return f();
         }
-        return -1;
-    }
+        __except ([&](EXCEPTION_POINTERS* eps) -> DWORD {
+            __try {
+                const EXCEPTION_RECORD& er = *eps->ExceptionRecord;
+                if (er.ExceptionCode == EXCEPTION_CPP_MICROSOFT) { // C++ exception
+                    UntypedException ue(er);
+                    if (ex = exception_cast<Ex>(ue, true)) {
+                        int skip = 2; // skip RaiseException and _CxxThrowException
+                        st.generate(*eps->ContextRecord, skip);
+                        return EXCEPTION_EXECUTE_HANDLER;
+                    }
+                }
+                return EXCEPTION_CONTINUE_SEARCH;
+            }
+            __except (EXCEPTION_EXECUTE_HANDLER) {
+                return EXCEPTION_CONTINUE_SEARCH;
+            }
+            }(GetExceptionInformation())) {
+            return [&] {
+                return e(*ex, st);
+            }();
+        }
+    }();
 }
-int main(int argc, char ** argv) {
+
+int main(int argc, char** argv) {
     SymInit sym;
-    std::string buffer;
-    return seh_helper(argc, argv, buffer);
+    return tryCatch<std::exception>([&]() -> int {
+        throw std::runtime_error("I'm an exception!");
+        }, [&](std::exception& ex, StackTrace const& st) {
+            std::cerr << ex.what() << "\n\nStack:\n" << st << std::endl;
+            return -1;
+        });
 }
